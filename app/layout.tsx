aa7f0a6c763048ceb734d05ec1575a7c9d3f72fc
Cenk Merk <cenkmerk@gmail.com>
2023-10-04 00:32:43 +0300

    Spinner added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
-import User from "@/components/User";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +17,6 @@ export default function RootLayout({
       <body className="flex justify-center relative">
         <Navbar />
         <main>{children}</main>
-        <User/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Posts from "@/components/Posts";
+import User from "@/components/User";
 
 export default function Home() {
-  return <Posts />;
+  return (
+    <>
+      <Posts />
+      <User />
+    </>
+  );
 }
